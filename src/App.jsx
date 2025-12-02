f14d6cd47054e3baba862057fcb76bfc2ed8904c
MirnaBuntic <mirnacb@hiof.no>
2025-12-02 23:33:36 +0100

    la till en import

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Routes, Route } from "react-router-dom"
 import Layout from './components/Layout';
 import Home from './components/Home';
